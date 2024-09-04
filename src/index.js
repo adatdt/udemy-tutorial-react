60e1b6791201971140c53f10be49a23d6f37758d
syahadat barkah <adatdt@gmail.com>
2024-09-04 22:13:30 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-function Greeting() {
-  return <h3>My First Component</h3>;
+
+import './index.css'
+const BookList = () =>{
+  return (<section className="bookList">
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+
+  </section>);
 }
 
+const Book = () =>{
+  return (<article className="book">
+    <Image />
+    <Title />
+    <Author />
+  </article>);
+}
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/71KTwO53SnL._AC_UL381_SR381,381_.jpg" 
+alt="Good Energy: The Surprising Connection Between Metabolism and Limitless Health"/>;
+const Title = () => <h2>Good Energy: The Surprising Connection Between Metabolism and Limitless Health</h2>;
+const Author = () => {
+  return <h4> Casey Means MD</h4>;
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting></Greeting>);
+root.render(<BookList/>);
