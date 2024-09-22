5e0cd2f28dfcfef0f888d3e4e3f900a4d5ac4a69
syahadat barkah <adatdt@gmail.com>
2024-09-22 20:16:44 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,29 +2,55 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import './index.css'
+
+const books = [
+  {
+    title : "Casey Means MD",
+    img: "/images/img1.jpg" ,
+    author : "sarah",
+    id:1
+  },
+  {
+    title : "Casey Means MD 2",
+    img: "/images/img1.jpg",
+    author : "jhon",
+    id:2 
+  }
+]
+
 const BookList = () =>{
   return (<section className="bookList">
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+    {books.map((book) => {
+      return <Book {...book} key={book.id}>
+    </Book>
+    })}    
 
   </section>);
 }
 
-const Book = () =>{
+const Book = (props) =>{
+  const {img, title, author} = props
   return (<article className="book">
-    <Image />
-    <Title />
-    <Author />
-  </article>);
+    <img src={img} 
+    alt="Good Energy: The Surprising "/>
+    <h2>{title}</h2>
+    <h4>{author}</h4>
+
+  </article>
+  
+  );
 }
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/71KTwO53SnL._AC_UL381_SR381,381_.jpg" 
-alt="Good Energy: The Surprising Connection Between Metabolism and Limitless Health"/>;
-const Title = () => <h2>Good Energy: The Surprising Connection Between Metabolism and Limitless Health</h2>;
+const Image = () => <img src="/images/img1.jpg" 
+alt="Good Energy: The Surprising "/>;
+const Title = () => <h2>Good Energy: The Surprising </h2>;
 const Author = () => {
-  return <h4> Casey Means MD</h4>;
+  const inlineHeadingStyles = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.5rem',
+  };
+  return <h4 style={inlineHeadingStyles} > Casey Means MD</h4>;
 }
 
 
